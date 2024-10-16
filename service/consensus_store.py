500c3d1db4ccc5cec7bb2007329421ea47b97a4c
happie-chappie <76691953+happie-chappie@users.noreply.github.com>
2024-10-16 11:43:10 +0530

    Update consensus_store.py

--- service/consensus_store.py
+++ service/consensus_store.py
@@ -1,5 +1,6 @@
-def achiece_consensus(consensus_type):
+def achiece_consensus(consensus_type, val_list):
   if consensus_type == 1:
     return pass
   if consensus_type == 1:
-    return pass
+    # if all values are same return pass else fail
+      
