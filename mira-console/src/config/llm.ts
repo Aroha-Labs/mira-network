6f74e7926f285f0beead3a50f9edf55c8823f009
Hamid Raza <contact@hamidraza.com>
2024-12-09 18:50:27 +0530

    feat: update LLM_BASE_URL and ROUTER_BASE_URL for local development and production environments

--- mira-console/src/config/llm.ts
+++ mira-console/src/config/llm.ts
@@ -1,6 +1,7 @@
 // export const LLM_BASE_URL = "https://mira-client-1.alts.dev/v1";
-export const LLM_BASE_URL = "http://localhost:11434/v1";
-// export const DEFAULT_MODEL = "mira/llama3:instruct";
-export const DEFAULT_MODEL = "llama3.2:3b-instruct-q8_0";
 
-export const ROUTER_BASE_URL = "https://mira-client-balancer.arohalabs.dev";
+// export const LLM_BASE_URL = "http://localhost:11434/v1";
+// export const DEFAULT_MODEL = "mira/llama3:instruct";
+
+export const ROUTER_BASE_URL = "https://mira-client-balancer.alts.dev";
+export const DEFAULT_MODEL = "llama3.2:3b-instruct-q8_0";
