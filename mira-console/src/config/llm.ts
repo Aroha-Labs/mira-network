e9ae63f73ae1fee5a878e9cd55f47d7dfcdcd776
Hamid Raza <contact@hamidraza.com>
2025-01-03 04:07:24 +0530

    feat(mira-console): add example .env file and update LLM configuration to use environment variables

--- mira-console/src/config/llm.ts
+++ mira-console/src/config/llm.ts
@@ -1,10 +1,3 @@
-// export const LLM_BASE_URL = "https://mira-client-1.alts.dev/v1";
-
-// export const ROUTER_BASE_URL = "http://localhost:8000";
-// // export const LLM_BASE_URL = "http://localhost:11434/v1";
-// export const LLM_BASE_URL = "http://localhost:8000/v1";
-// export const DEFAULT_MODEL = "mira/llama3:instruct";
-
-export const ROUTER_BASE_URL = "https://mira-client-balancer.alts.dev";
-export const LLM_BASE_URL = "https://mira-client-balancer.alts.dev/v1";
-export const DEFAULT_MODEL = "llama3.2:3b-instruct-q8_0";
+export const ROUTER_BASE_URL = import.meta.env.VITE_ROUTER_BASE_URL;
+export const LLM_BASE_URL = import.meta.env.VITE_LLM_BASE_URL;
+export const DEFAULT_MODEL = import.meta.env.VITE_DEFAULT_MODEL;
