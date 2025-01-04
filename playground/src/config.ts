f6ae5708f14ce06f0cb15bc8cf5a05b17c2e721f
Hamid Raza <contact@hamidraza.com>
2025-01-04 06:50:53 +0530

    feat: add environment variables for API and LLM base URLs

--- playground/src/config.ts
+++ playground/src/config.ts
@@ -1,5 +1,8 @@
-export const API_BASE_URL = "https://mira-client-balancer.alts.dev";
-export const LLM_BASE_URL = "https://mira-client-balancer.alts.dev/v1";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+export const LLM_BASE_URL = process.env.NEXT_PUBLIC_LLM_BASE_URL!;
+
+// export const API_BASE_URL = "https://mira-client-balancer.alts.dev";
+// export const LLM_BASE_URL = "https://mira-client-balancer.alts.dev/v1";
 
 // export const API_BASE_URL = "http://localhost:8000";
 // export const LLM_BASE_URL = "http://localhost:8000/v1";
