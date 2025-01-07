9dd1220434875217ced452c1e71f03630680f064
Hamid Raza <contact@hamidraza.com>
2025-01-07 08:39:36 +0530

    feat: implement database migration setup with Alembic and remove unused database creation code

--- router/src/mira_client_dashboard/main.py
+++ router/src/mira_client_dashboard/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.mira_client_dashboard.api.v1 import router as v1_router
 from src.mira_client_dashboard.api.admin import router as admin_router
-from src.mira_client_dashboard.db.base import engine
-from sqlmodel import SQLModel
 import uvicorn
 from prometheus_fastapi_instrumentator import Instrumentator
 from scalar_fastapi import get_scalar_api_reference
@@ -44,9 +42,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Create tables
-# SQLModel.metadata.create_all(engine)
 
 # Include routers
 app.include_router(v1_router)
