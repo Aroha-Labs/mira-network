84b2d101c9e5db76ad7ec031e7e255100982f28b
rachit1994 <rachit.srivastava1994@gmail.com>
2025-01-09 20:11:42 +0530

    dashboard revamp

--- playground_v2/src/components/dashboard/Network.tsx
+++ playground_v2/src/components/dashboard/Network.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Card from "src/components/card";
+import useMachine from "src/hooks/useMachine";
 import { Button } from "../button";
 
 const Network = () => {
+  const { onlineMachinesCount } = useMachine();
   return (
     <Card>
       <div className="flex justify-between items-center p-4">
@@ -13,7 +15,7 @@ const Network = () => {
         </Link>
 
         <p className="text-[#303030] text-sm font-bold opacity-50">
-          -- / -- / --
+          {onlineMachinesCount} NODES LIVE
         </p>
       </div>
     </Card>
