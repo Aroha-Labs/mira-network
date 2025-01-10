d18bc0a0f47f301e1be77e04984886d67b09f62a
rachit1994 <rachit.srivastava1994@gmail.com>
2025-01-10 23:05:05 +0530

    responsiveness

--- playground_v2/src/components/ApiKeys/AddApiKey.tsx
+++ playground_v2/src/components/ApiKeys/AddApiKey.tsx
@@ -6,6 +6,7 @@ import {
   PopoverTrigger,
 } from "src/components/popover";
 import useApiTokens from "src/hooks/useApiTokens";
+import Card from "../card";
 
 const AddApiKey = () => {
   const { addApiKey } = useApiTokens();
@@ -22,56 +23,57 @@ const AddApiKey = () => {
       <PopoverTrigger asChild>
         <Button onClick={() => setIsModalOpen(true)}>Add API Key</Button>
       </PopoverTrigger>
-      <PopoverContent className="w-80">
-        <p className="text-md leading-[22px] tracking-[-0.013em] w-full text-center p-4">
-          Create a new secret key
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label
-            htmlFor="description"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Name
-          </label>
-          <input
-            id="description"
-            type="text"
-            placeholder="secret-key-1"
-            className="mt-1 block w-full px-3 py-2 border border-[#D7E2DE] shadow-sm focus:outline-none focus:border-[#308F6A] focus:border-blue-500 sm:text-sm"
-          />
-          {addApiKey?.isError && (
-            <div className="text-red-600 mb-4 font-light text-sm mt-2">
-              {addApiKey?.error?.message}
-            </div>
-          )}
-
-          {addApiKey.isSuccess && (
-            <div className="text-green-600 mb-4 font-light text-sm mt-2">
-              API key created successfully
-            </div>
-          )}
-
-          <div className="flex items-center gap-2 mt-4 w-full">
-            <Button
-              variant="secondary"
-              type="button"
-              onClick={(e) => {
-                e.preventDefault();
-                setIsModalOpen(false);
-              }}
-            >
-              Cancel
-            </Button>
-            <Button
-              type="submit"
-              className="w-full"
-              loading={addApiKey.isPending}
-            >
-              Create Key
-            </Button>
+      <PopoverContent className="min-w-80 p-0">
+        <Card>
+          <div className="flex items-center justify-between">
+            <p className="text-md leading-[22px] tracking-[-0.013em] pl-0 pr-4 pt-4 pb-4">
+              &lt; Create new key
+            </p>
+            <div className="flex-grow border-t w-full border-dashed border-[#9CB9AE] mx-1 flex-1 h-[2px]" />
           </div>
-        </form>
+
+          <form onSubmit={handleSubmit} className="py-4">
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-[#9C9B9B]"
+            >
+              name
+            </label>
+            <input
+              id="description"
+              type="text"
+              placeholder="secret-key-1"
+              className="mt-1 block w-full px-3 py-2 border border-[#D7E2DE] shadow-sm focus:outline-none focus:border-[#308F6A] focus:border-blue-500 sm:text-sm"
+            />
+            {addApiKey?.isError && (
+              <div className="text-red-600 mb-4 font-light text-sm mt-2">
+                {addApiKey?.error?.message}
+              </div>
+            )}
+
+            {addApiKey.isSuccess && (
+              <div className="text-green-600 mb-4 font-light text-sm mt-2">
+                API key created successfully
+              </div>
+            )}
+
+            <div className="flex items-center gap-2 mt-4 w-full">
+              <Button type="submit" loading={addApiKey.isPending}>
+                Create Key
+              </Button>
+              <Button
+                variant="secondary"
+                type="button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsModalOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Card>
       </PopoverContent>
     </Popover>
   );
