0c870880295c2b15e126739965ea1b85cd1a8b90
Hamid Raza <contact@hamidraza.com>
2025-01-10 15:21:55 +0530

    feat: enhance API logging to include model pricing and request payload

--- router/src/router/api/v1/network.py
+++ router/src/router/api/v1/network.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, Response, HTTPException
 from fastapi.responses import StreamingResponse
 from sqlmodel import Session, select
-from src.router.core.types import User
+from src.router.core.types import ModelPricing, User
 from src.router.models.logs import ApiLogs
+from src.router.models.system_settings import SystemSettings
 from src.router.db.session import get_session
 from src.router.core.security import verify_user
 from src.router.utils.network import get_random_machines, PROXY_PORT
@@ -120,19 +121,42 @@ async def generate(
                 if "usage" in json_line:
                     usage = json_line["usage"]
             except json.JSONDecodeError as e:
-                print(e)
+                # print(e)
+                pass
             yield line
+
+        mp = db.exec(
+            select(SystemSettings).where(SystemSettings.name == "MODEL_PRICING")
+        ).first()
+
+        if mp is None:
+            # throw an error
+            raise HTTPException(
+                status_code=500, detail="MODEL_PRICING not set in system settings"
+            )
+
+        model_p = mp.value.get(req.model)
+
+        if model_p is None:
+            raise HTTPException(
+                status_code=500, detail="Model pricing not set in system settings"
+            )
+
+        model_pricing = ModelPricing(**model_p)
 
         # Log the request
         api_log = ApiLogs(
             user_id=user.id,
             payload=req.model_dump_json(),
+            request_payload=req.model_dump(),
             response=result_text,
             prompt_tokens=usage.get("prompt_tokens", 0),
             completion_tokens=usage.get("completion_tokens", 0),
             total_tokens=usage.get("total_tokens", 0),
             total_response_time=time.time() - timeStart,
             model=req.model,
+            model_pricing=model_pricing,
+            machine_id=machine.machine_uid,
         )
 
         db.add(api_log)
