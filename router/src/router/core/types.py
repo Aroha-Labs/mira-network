59007b77a2da1321df3b477715c47155da263083
Hamid Raza <contact@hamidraza.com>
2025-01-12 20:02:13 +0530

    feat: add optional api_key_id field to User model for enhanced flexibility

--- router/src/router/core/types.py
+++ router/src/router/core/types.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Optional
 from gotrue import User as GoUser
 from pydantic import BaseModel
 
 
 class User(GoUser):
     roles: List[str] = []
+    api_key_id: Optional[int] = None
 
 
 class ModelPricing(BaseModel):
