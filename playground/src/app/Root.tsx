27e8ab21094cd1d571299b6fae11e56ac5400c18
Hamid Raza <contact@hamidraza.com>
2025-01-12 20:03:09 +0530

    feat: refactor header component into separate Header file for improved structure and maintainability

--- playground/src/app/Root.tsx
+++ playground/src/app/Root.tsx
@@ -4,9 +4,9 @@ import c from "clsx";
 import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link"; // Import Link from next/link
 import LayoutChildren from "./LayoutChildren";
 import { Toaster } from "react-hot-toast";
+import { Header } from "src/components/Header";
 
 const queryClient = new QueryClient();
 
@@ -50,32 +50,8 @@ const Root = ({ children }: RootProps) => {
         )} // Tailwind's text-base sets the font size to 14px
       >
         <QueryClientProvider client={queryClient}>
-          <header className="sticky top-0 z-10 flex items-center justify-center gap-2 p-4 bg-white border-b border-gray-300">
-            <Link href="/" className="flex items-center gap-2">
-              <img src="/img/logo.svg" alt="Mira" />
-              <h3 className="text-lg">console</h3>
-              <span className="text-xs bg-gray-200 px-2 py-0.5 rounded-full text-gray-600">
-                Beta
-              </span>
-            </Link>
-            {/* Tailwind's text-lg sets the font size to 18px */}
-            <div className="flex-1"></div>
-            <Link href="/chat" className="flex text-blue-500 hover:underline">
-              Console
-            </Link>
-            <Link
-              href="/terminal"
-              className="flex ml-4 text-blue-500 hover:underline"
-            >
-              Terminal
-            </Link>
-            <Link
-              href="/admin"
-              className="flex ml-4 text-blue-500 hover:underline"
-            >
-              Admin
-            </Link>
-          </header>
+          <Header />
+
           {!isOnline && (
             <div className="relative px-4 py-3 text-center text-red-700 bg-red-100 border border-red-400 rounded">
               <span className="font-medium">No internet connection.</span>
