02644b3a15f3f7f6eba9b194129023553f324d02
rachit1994 <rachit.srivastava1994@gmail.com>
2025-01-14 13:37:08 +0530

    loggedout change

--- playground_v2/src/app/LayoutChildren.tsx
+++ playground_v2/src/app/LayoutChildren.tsx
@@ -1,4 +1,6 @@
 import { useStore } from "@tanstack/react-store";
+import Link from "next/link";
+import Card from "src/components/card";
 import { useLogout } from "src/hooks/useLogout";
 import { usePermissions } from "src/hooks/usePermissions";
 import Loading from "src/pageLoading/dashboard";
@@ -21,28 +23,25 @@ const LayoutChildren = ({ children }: LayoutChildrenProps) => {
 
   if (session && !hasPermission) {
     return (
-      <div className="flex items-center justify-center flex-1">
-        <div
-          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert"
-        >
-          <strong className="font-bold">Error: </strong>
-          <div>You don&apos;t have permission to access this page</div>
-          <button
-            onClick={() => logoutMutation.mutate()}
-            className={`mt-4 px-4 py-2 rounded text-white ${
-              logoutMutation.isPending ? "bg-gray-500" : "bg-red-500"
-            }`}
-            disabled={logoutMutation.isPending}
-          >
-            {logoutMutation.isPending ? "Logging out..." : "Logout"}
-          </button>
-          {logoutMutation.isError && (
-            <div className="mt-2 text-red-700">
-              Error: {logoutMutation.error.message}
+      <div className="flex items-center justify-center flex-1 w-[400px]">
+        <Card>
+          <div className="pl-[20px] pr-[20px] pt-[14px] pb-[32px]">
+            <div>
+              Welcome to the Voyager Testnet. To get access and free credits,
+              please join our Discord server and send us a message there.
             </div>
-          )}
-        </div>
+            <div className="mt-8">
+              <Link
+                href="https://discord.com/invite/mira-network"
+                target="_blank"
+                className="px-4 py-2 text-white bg-black"
+                onClick={() => logoutMutation.mutate()}
+              >
+                Join Discord
+              </Link>
+            </div>
+          </div>
+        </Card>
       </div>
     );
   }
