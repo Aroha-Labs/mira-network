1e1852085ecbab24f031eef58e6d3bd1aefaf9c4
Hamid Raza <contact@hamidraza.com>
2025-01-16 16:00:25 +0530

    refactor user credits retrieval to use updated user model

--- router/src/router/api/v1/credits.py
+++ router/src/router/api/v1/credits.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from src.router.core.types import User
-from src.router.models.user import UserCredits, UserCreditsHistory
+from src.router.models.user import User as UserModel, UserCreditsHistory
 from src.router.db.session import get_session
 from src.router.core.security import verify_user
 
@@ -12,10 +12,8 @@ router = APIRouter()
 def get_user_credits(
     db: Session = Depends(get_session), user: User = Depends(verify_user)
 ):
-    user_credits = db.exec(
-        select(UserCredits).where(UserCredits.user_id == user.id)
-    ).first()
-    return {"credits": user_credits.credits if user_credits else 0}
+    user_data = db.exec(select(UserModel).where(UserModel.user_id == user.id)).first()
+    return {"credits": user_data.credits if user_data else 0}
 
 
 @router.get("/user-credits-history")
