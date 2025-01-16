0ad73be07340e591b2d4d963f2f1a6e8c7dfaf25
Hamid Raza <contact@hamidraza.com>
2025-01-16 15:59:00 +0530

    merge user credits into users table and update user model

--- router/src/router/models/__init__.py
+++ router/src/router/models/__init__.py
@@ -2,13 +2,12 @@ from .flows import Flows
 from .tokens import ApiToken
 from .logs import ApiLogs
 from .machines import Machine
-from .user import User, UserCredits, UserCreditsHistory
+from .user import User, UserCreditsHistory
 from .system_settings import SystemSettings
 
 __all__ = [
     "Flows",
     "ApiToken",
-    "UserCredits",
     "UserCreditsHistory",
     "User",
     "ApiLogs",
