0e87809ec516f467e6f487157fc4aa6e9fc517c2
Hamid Raza <contact@hamidraza.com>
2025-01-18 00:39:29 +0530

    Add fields to Machine model and update migration script
    - Introduced new fields: name, description, auth_tokens, and disabled to the Machine model.
    - Updated the migration script to add these fields to the database schema.
    - Adjusted indexing for network_ip and network_machine_uid for improved query performance.

--- router/src/router/models/machines.py
+++ router/src/router/models/machines.py
@@ -1,11 +1,21 @@
-from sqlalchemy import func
-from sqlmodel import SQLModel, Field as SQLField
+from typing import Dict, Any
+from sqlalchemy import Column, func
+from sqlmodel import SQLModel, Field
+from sqlalchemy.dialects.postgresql import JSONB, BOOLEAN
 from datetime import datetime
 
 
 class Machine(SQLModel, table=True):
-    id: int | None = SQLField(default=None, primary_key=True)
-    network_machine_uid: str = SQLField(index=True)
-    network_ip: str = SQLField(index=True)
-    created_at: datetime = SQLField(default=func.now(), nullable=False)
-    updated_at: datetime = SQLField(default=func.now(), nullable=False)
+    id: int | None = Field(default=None, primary_key=True)
+    network_machine_uid: str = Field(index=True, unique=True)
+    name: str | None = Field(index=True)
+    description: str | None = Field(index=True)
+    auth_tokens: Dict[str, Any] = Field(
+        sa_column=Column(JSONB, server_default="{}", nullable=False)
+    )
+    disabled: bool = Field(
+        sa_column=Column(BOOLEAN, server_default="False", nullable=False)
+    )
+    network_ip: str = Field(index=True, unique=True)
+    created_at: datetime = Field(default=func.now(), nullable=False)
+    updated_at: datetime = Field(default=func.now(), nullable=False)
