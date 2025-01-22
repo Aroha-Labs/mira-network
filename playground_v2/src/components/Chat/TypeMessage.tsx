ffdda8da9969a7d08e686c72e6381bd1dcfc0618
rachit1994 <rachit.srivastava1994@gmail.com>
2025-01-23 01:08:38 +0530

    chat new screen

---
+++ playground_v2/src/components/Chat/TypeMessage.tsx
@@ -0,0 +1,31 @@
+import { TextArea } from "src/components/TextArea";
+import { Button } from "src/components/button";
+
+const TypeMessage = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const message = formData.get("message") as string;
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex gap-2 w-[480px] justify-center items-start absolute p-[12px] bottom-0 right-0 border-[#306E564D] bg-[#F1F7F5]"
+    >
+      <TextArea
+        name="message"
+        className="flex-1 p-[8px]"
+        placeholder="type your message"
+      />
+      <Button
+        type="submit"
+        className="bg-[#308F6A] text-white w-[51px] h-[28px] text-[13px] font-medium leading-[15.6px] tracking-[-0.02em] text-left decoration-skip-ink-none"
+      >
+        send
+      </Button>
+    </form>
+  );
+};
+
+export default TypeMessage;
