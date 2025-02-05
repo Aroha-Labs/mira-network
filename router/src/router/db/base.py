02d48485c5b9ec660e301b8c0b5b95d39592678d
sarim2000 <sarimbleedblue@gmail.com>
2025-02-06 01:59:13 +0530

    revert db

--- router/src/router/db/base.py
+++ router/src/router/db/base.py
@@ -3,5 +3,8 @@ from sqlmodel import create_engine
 
 engine = create_engine(
     os.getenv("DB_CONNECTION_STRING"),
-    # connect_args={"sslmode": "false"},
+    connect_args={"sslmode": "require"},
+    pool_size=0,  # ✅ Disable SQLAlchemy's internal pool
+    max_overflow=0,  # ✅ Prevent overflow since RDS Proxy handles it
+    pool_pre_ping=True,  # ✅ Ensures stale connections are dropped
 )
