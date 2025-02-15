5b20a3e6d774edfe0f900c6c04ecf1d94f175334
Hamid Raza <contact@hamidraza.com>
2025-02-15 14:01:31 +0500

    refactor(service/config): simplify environment variable handling by using a class

--- service/config.py
+++ service/config.py
@@ -1,20 +1,6 @@
 import os
-import ipaddress
-import sys
 
 
-def validate_ip(ip: str | None) -> str:
-    if not ip:
-        print("ERROR: MACHINE_IP environment variable is not set")
-        sys.exit(1)
-
-    try:
-        ipaddress.ip_address(ip)
-        return ip
-    except ValueError:
-        print(f"ERROR: Invalid IP address: {ip}")
-        sys.exit(1)
-
-
-MACHINE_IP = validate_ip(os.getenv("MACHINE_IP"))
-MACHINE_API_TOKEN = os.getenv("MACHINE_API_TOKEN")
+class Env:
+    MACHINE_IP = os.getenv("MACHINE_IP")
+    MACHINE_API_TOKEN = os.getenv("MACHINE_API_TOKEN")
