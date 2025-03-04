16d3d9b06c66575815e2a69889071e5848011331
Hamid Raza <contact@hamidraza.com>
2025-03-04 07:41:05 +0300

    refactor(terminal, chat): simplify ReactMarkdown usage and remove unused code

--- playground/src/components/ReactMarkdown.tsx
+++ playground/src/components/ReactMarkdown.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+// type CodeProps = ComponentPropsWithoutRef<"code"> & { inline?: boolean };
+
 interface ReactMarkdownProps extends React.ComponentProps<typeof Markdown> {
   children: string;
 }
@@ -9,10 +11,38 @@ interface ReactMarkdownProps extends React.ComponentProps<typeof Markdown> {
 const ReactMarkdown = ({
   children,
   remarkPlugins = [],
+  components = {},
   ...props
 }: ReactMarkdownProps) => {
   return (
-    <Markdown remarkPlugins={[remarkGfm, ...remarkPlugins!]} {...props}>
+    <Markdown
+      remarkPlugins={[remarkGfm, ...remarkPlugins!]}
+      components={{
+        // pre: ({ node, children, ...props }) => (
+        //   <div className="relative group">
+        //     <pre {...props}>{children}</pre>
+        //     <button
+        //       onClick={() => {
+        //         console.log(children.props.children);
+
+        //         const preElement = children?.toString() || "";
+        //         navigator.clipboard.writeText(preElement);
+        //       }}
+        //       className="absolute p-1 text-gray-200 transition-opacity bg-gray-700 rounded-sm opacity-0 top-2 right-2 group-hover:opacity-100"
+        //     >
+        //       <ClipboardIcon className="w-4 h-4" />
+        //     </button>
+        //   </div>
+        // ),
+        // code: ({ inline, className, children, ...props }: CodeProps) => (
+        //   <code className={className} {...props}>
+        //     {children}
+        //   </code>
+        // ),
+        ...components,
+      }}
+      {...props}
+    >
       {children}
     </Markdown>
   );
